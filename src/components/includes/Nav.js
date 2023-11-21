eae9c19bba030b04ebf2d9aae91b38b91687bdd2
Jaryan / Omid Moghadam <tariux@protonmail.com>
2023-11-21 11:53:28 +0330

    Finally, we are close to the goal!
    Project search page was added and connected to the backend and core of the application

--- src/components/includes/Nav.js
+++ src/components/includes/Nav.js
@@ -1,60 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
+import { FaSearchDollar , FaRegBell , FaHome , FaRegSun , FaRegUser   } from "react-icons/fa";
+
+
 function Nav() {
     return (
         <nav className="blocks">
-
-            <Link to='/searchproject'>
-
-                <a href="#" className="block">
+                        <Link to='/'>
+            <a href="#" className="block">
                     <div className="block__item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
-                        </svg>
+                    <FaHome fill='white' />
+
+                    </div>
+                </a>
+            </Link>
+            <Link to='/searchproject'>
+            <a href="#" className="block">
+                    <div className="block__item">
+                    <FaSearchDollar fill='white' />
+
                     </div>
                 </a>
             </Link>
             <Link to='/notifications'>
-                <a href="#" className="block">
+            <a href="#" className="block">
                     <div className="block__item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
-                        </svg>
+                    <FaRegBell fill='white' />
+
                     </div>
                 </a>
             </Link>
-
-            <Link to='/recommended'>
-
-                <a href="#" className="block">
-                    <div className="block__item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
-                        </svg>
-                    </div>
-                </a>
-
-            </Link>
-
             <Link to='/setting'>
-
-                <a href="#" className="block">
+            <a href="#" className="block">
                     <div className="block__item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                        </svg>
+                    <FaRegSun  fill='white' />
+
                     </div>
                 </a>
             </Link>
 
             <Link to='/auth'>
-
-                <a href="#" className="block">
+            <a href="#" className="block">
                     <div className="block__item">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                        </svg>
+                    <FaRegUser  fill='white' />
+
                     </div>
                 </a>
             </Link>
--- src/router/Tabs.js
+++ src/router/Tabs.js
@@ -8,7 +8,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from '../components/Home'
 import Setting from '../components/Setting'
 import SearchProject from '../components/SearchProject'
-import RecommendedProjects from '../components/RecommendedProjects'
 import Notifications from '../components/Notifications'
 import Auth from '../components/Auth'
 import Header from '../components/includes/Header';
@@ -24,7 +23,6 @@ function Tabs(params) {
             <Route path='/' element={<Home />} />
             <Route path="setting" element={<Setting />} />
             <Route path="searchproject" element={<SearchProject />} />
-            <Route path="recommended" element={<RecommendedProjects />} />
             <Route path="notifications" element={<Notifications />} />
             <Route path="auth" element={<Auth />} />
         </Routes>
